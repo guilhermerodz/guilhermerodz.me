a208287379789c2d4d2b021efef3dad8a16599a8
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-10-30 01:02:39 -0300

    feat: Added constants

---
+++ src/styles/constants.js
@@ -0,0 +1,48 @@
+// --------------------------------------------------
+// Sizes
+// --------------------------------------------------
+
+export const Sizes = {
+  xxs: '.4rem',
+  xs: '.8rem',
+  sm: '1.6rem',
+  default: '2.4rem',
+  md: '3.2rem',
+  lg: '4.8rem',
+  xlg: '6.4rem',
+  xxlg: '9.6rem',
+};
+
+export const Heights = {
+  headerSm: Sizes.lg,
+  headerLg: Sizes.xlg,
+  footer: '4rem',
+  mainTop: Sizes.md,
+};
+
+// --------------------------------------------------
+// Colors
+// --------------------------------------------------
+
+export const Color = {
+  primary: '#eee',
+  secondary: '#eee',
+  tertiary: '#eee',
+  quaternary: '#eee',
+  quinary: '#eee',
+  senary: '#eee',
+  septenary: '#eee',
+  octonary: '#eee',
+  nonary: '#eee',
+};
+
+// --------------------------------------------------
+// z-index
+// --------------------------------------------------
+
+export const StackOrder = {
+  background: '-1',
+  default: '1',
+  footer: '5',
+  header: '5',
+};
